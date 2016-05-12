4d163933598af3fa0def122c3d58dd34de8d85a9
thejesusof <thejesusof@gmail.com>
2016-05-12 20:27:20 +0300

    Tests for reduce function

--- tests.js
+++ tests.js
@@ -3,10 +3,20 @@ describe("map", function() {
 
   it("Should return passed collection if a given function is an identity-function.", function () {
     assert.deepEqual(map(simple_test_array, function(el) {return el;}), simple_test_array);
-  })
+  });
 
   it("It should maps collection based on a passed function.", function() {
     assert.deepEqual(map(simple_test_array, function(el) {return el * 2;}), [2, 4, 6]);
+  });
+
+});
+
+describe("reduce", function() {
+  simple_test_array = [1, 2, 3];
+
+  it("Applies a function to each element of the collection and rolls a result, starting from the start.", function () {
+    assert.deepEqual(reduce(simple_test_array, function(memo, num){ return memo + num; }, 0), 6);
+    assert.deepEqual(reduce(simple_test_array, function(memo, num){ return memo + num; }, 4), 10);
   });
 
 });
